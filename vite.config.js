6f9b300fc860d9c4d45d015afdeb0383cfa84354
kevinvillajim <kevinvillajim@hotmail.com>
2024-04-06 18:14:19 -0500

    Nuevos Modulos

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "",
+  plugins: [react()],
+  base: "https://kevinvillajim.github.io/BlagetSystem/",
 });
