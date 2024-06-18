dc87e41bcfc574dbfeb6520962b65d8e31d879d2
kevinvillajim <kevinvillajim@hotmail.com>
2024-06-17 22:35:16 -0500

    Curso 1 Terminados

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,111 +1,96 @@
-import { useState } from "react";
+import {useState} from "react";
 import {
-  Button,
-  FormControl,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-  Typography,
-  Box,
-  CssBaseline,
-  ThemeProvider,
-  createTheme,
+	Button,
+	FormControl,
+	FormControlLabel,
+	Radio,
+	RadioGroup,
+	Typography,
+	Box,
+	CssBaseline,
+	ThemeProvider,
+	createTheme,
 } from "@mui/material";
-import cursos from "./cursos";
 
 const theme = createTheme({
-  palette: {
-    mode: "dark",
-    text: {
-      primary: "#ffffff",
-    },
-    primary: {
-      main: "#00ff00", // Color verde para botones y radios
-    },
-  },
+	palette: {
+		mode: "dark",
+		text: {
+			primary: "#ffffff",
+		},
+		primary: {
+			main: "#00ff00", // Color verde para botones y radios
+		},
+	},
 });
 
-const Quiz = ({ questions }) => {
-  const initialAnswers = questions.map(() => null);
-  const [answers, setAnswers] = useState(initialAnswers);
-  const [score, setScore] = useState(null);
+const Quiz = ({questions}) => {
+	const initialAnswers = questions.map(() => null);
+	const [answers, setAnswers] = useState(initialAnswers);
+	const [score, setScore] = useState(null);
 
-  const handleAnswerChange = (questionIndex, optionIndex) => {
-    setAnswers({ ...answers, [questionIndex]: optionIndex });
-  };
+	const handleAnswerChange = (questionIndex, optionIndex) => {
+		setAnswers({...answers, [questionIndex]: optionIndex});
+	};
 
-  const handleSubmit = () => {
-    let score = 0;
-    questions.forEach((question, index) => {
-      if (answers[index] === question.answer - 1) {
-        score += 1;
-      }
-    });
-    setScore(score);
-    if (score === questions.length) {
-      localStorage.setItem("unit1", "true");
-    } //Cambiar por algo mas
-  };
+	const handleSubmit = () => {
+		let score = 0;
+		questions.forEach((question, index) => {
+			if (answers[index] === question.answer - 1) {
+				score += 1;
+			}
+		});
+		setScore(score);
+		if (score === questions.length) {
+			localStorage.setItem("unit1", "true");
+		} //Cambiar por algo mas
+	};
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box
-        p={4}
-        sx={{ color: "white" }}
-      >
-        <Typography
-          variant="h4"
-          gutterBottom
-        >
-          Cuestionario sobre Protección de Datos Personales
-        </Typography>
-        {questions.map((question, index) => (
-          <Box
-            key={index}
-            mb={4}
-          >
-            <Typography variant="h6">{question.question}</Typography>
-            <FormControl component="fieldset">
-              <RadioGroup
-                name={`question-${index}`}
-                value={answers[index]}
-                onChange={(e) =>
-                  handleAnswerChange(index, parseInt(e.target.value))
-                }
-              >
-                {question.options.map((option, optionIndex) => (
-                  <FormControlLabel
-                    key={optionIndex}
-                    value={optionIndex}
-                    control={<Radio sx={{ color: "white" }} />}
-                    label={
-                      <Typography sx={{ color: "white" }}>{option}</Typography>
-                    }
-                  />
-                ))}
-              </RadioGroup>
-            </FormControl>
-          </Box>
-        ))}
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-        >
-          Enviar
-        </Button>
-        {score !== null && (
-          <Typography
-            variant="h6"
-            mt={4}
-          >
-            Tu puntuación: {score} / {questions.length}
-          </Typography>
-        )}
-      </Box>
-    </ThemeProvider>
-  );
+	return (
+		<div className="my-[5rem] bg-gray-800 px-[5rem]">
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Box p={4} sx={{color: "white"}}>
+					<Typography variant="h4" gutterBottom>
+						Cuestionario sobre Protección de Datos Personales
+					</Typography>
+					{questions.map((question, index) => (
+						<Box key={index} mb={4}>
+							<Typography variant="h6">{question.question}</Typography>
+							<FormControl component="fieldset">
+								<RadioGroup
+									name={`question-${index}`}
+									value={answers[index]}
+									onChange={(e) =>
+										handleAnswerChange(index, parseInt(e.target.value))
+									}
+								>
+									{question.options.map((option, optionIndex) => (
+										<FormControlLabel
+											key={optionIndex}
+											value={optionIndex}
+											control={<Radio sx={{color: "white"}} />}
+											label={
+												<Typography sx={{color: "white"}}>{option}</Typography>
+											}
+										/>
+									))}
+								</RadioGroup>
+							</FormControl>
+						</Box>
+					))}
+					<Button variant="contained" color="primary" onClick={handleSubmit}>
+						Enviar
+					</Button>
+					{score !== null && (
+						<Typography variant="h6" mt={4}>
+							Tu puntuación: {score} / {questions.length}
+						</Typography>
+					)}
+				</Box>
+			</ThemeProvider>
+		</div>
+	);
 };
 
 export default Quiz;
--- src/views/Estudiante/Cursos.jsx
+++ src/views/Estudiante/Cursos.jsx
@@ -1,29 +1,52 @@
+import {useLayoutEffect} from "react";
 import SideBarCourse from "../../components/SideBarCourse";
 import HeaderCourse from "../../components/HeaderCourse";
 import ContentCourse from "../../components/ContentCourse";
-import { useState } from "react";
 import cursos from "../../components/cursos";
 
-export default function Cursos({ curso, contenido }) {
-  const [showModal, setShowModal] = useState(true);
-  return (
-    <>
-      <div>
-        <HeaderCourse title={cursos[curso].title} />
-        <div className="flex">
-          <div className={`w-[30%] ${showModal ? "block" : "hidden"}`}>
-            <SideBarCourse cursos={cursos[curso]} />
-          </div>
-          <div className="w-[100%] bg-[#191b20]">
-            <ContentCourse
-              subtitle={cursos[curso].title}
-              content={<>{contenido}</>}
-              showModal={showModal}
-              setShowModal={setShowModal}
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+const Cursos = ({curso, contenido}) => {
+	useLayoutEffect(() => {
+		const progressBar = document.querySelector(".progress__bar");
+
+		const updateProgress = () => {
+			if (progressBar) {
+				const body = document.body;
+				let scrollPos =
+					(window.scrollY / (body.scrollHeight - window.innerHeight)) * 100;
+				progressBar.style.width = `${scrollPos}%`;
+				console.log(`Scroll progress: ${scrollPos}%`);
+				requestAnimationFrame(updateProgress);
+			}
+		};
+
+		updateProgress();
+
+		// Clean up function
+		return () => {
+			if (progressBar) {
+				progressBar.style.width = "0";
+			}
+		};
+	}, []); // Empty dependency array ensures this effect runs only once
+
+	return (
+		<>
+			<div>
+				<HeaderCourse title={cursos[curso].title} />
+				<div className="flex">
+					<div className="w-[30%]">
+						<SideBarCourse cursos={cursos[curso]} />
+					</div>
+					<div className="w-[100%] bg-[#191b20]">
+						<ContentCourse
+							subtitle={cursos[curso].title}
+							content={<>{contenido}</>}
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Cursos;
