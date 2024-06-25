b53044624b7c2b0754c14a109fbe424fb8422731
kevinvillajim <kevinvillajim@hotmail.com>
2024-06-25 14:47:09 -0500

    Profile 100%

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import {ThemeProvider} from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "./components/Color";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import Dashboard from "./views/admin/Dashboard";
@@ -20,61 +20,102 @@ import DashboardEstudiante from "./views/Estudiante/DashboardEstudiante";
 import Certificados from "./views/admin/Certificados";
 import CertificadosEstudiante from "./views/Estudiante/CertificadosEstudiante";
 import Perfil from "./views/Perfil";
+import EditarPerfil from "./views/EditarPerfil";
 
 function App() {
-	return (
-		<>
-			{/* <ThemeProvider theme={theme}>
+  return (
+    <>
+      {/* <ThemeProvider theme={theme}>
         <RouterProvider router={router} />
       </ThemeProvider> */}
-			<ThemeProvider theme={theme}>
-				<Router>
-					<Routes>
-						<Route path="/home" element={<Home />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/admin/dashboard" element={<Dashboard />} />
-						<Route path="/admin/maestros" element={<Maestros />} />
-						<Route path="/admin/alumnos" element={<Alumnos />} />
-						<Route path="/admin/clases" element={<Clases />} />
-						<Route path="/admin/usuarios" element={<Usuarios />} />
-						<Route path="/admin/tareas" element={<Tareas />} />
-						<Route path="/admin/finanzas" element={<Finanzas />} />
-						<Route path="/admin/certificados" element={<Certificados />} />
-						<Route
-							path="/estudiante/cursos/curso1/unidad1"
-							element={<Unidad1 />}
-						/>
-						<Route
-							path="/estudiante/cursos/curso1/unidad2"
-							element={<Unidad2 />}
-						/>
-						<Route
-							path="/estudiante/cursos/curso1/unidad3"
-							element={<Unidad3 />}
-						/>
-						<Route
-							path="/estudiante/dashboard"
-							element={<DashboardEstudiante />}
-						/>
-						<Route
-							path="/estudiante/certificados"
-							element={<CertificadosEstudiante />}
-						/>
-						<Route
-							path="/estudiante/cursos/curso1/certificado"
-							element={<Certificado />}
-						/>
-						<Route
-							path="/estudiante/cursos/curso1"
-							element={<CursosRedirect />}
-						/>
-						<Route path="/profile" element={<Perfil />} />
-						<Route path="/" element={<Home />} />
-					</Routes>
-				</Router>
-			</ThemeProvider>
-		</>
-	);
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route
+              path="/home"
+              element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/admin/dashboard"
+              element={<Dashboard />}
+            />
+            <Route
+              path="/admin/maestros"
+              element={<Maestros />}
+            />
+            <Route
+              path="/admin/alumnos"
+              element={<Alumnos />}
+            />
+            <Route
+              path="/admin/clases"
+              element={<Clases />}
+            />
+            <Route
+              path="/admin/usuarios"
+              element={<Usuarios />}
+            />
+            <Route
+              path="/admin/tareas"
+              element={<Tareas />}
+            />
+            <Route
+              path="/admin/finanzas"
+              element={<Finanzas />}
+            />
+            <Route
+              path="/admin/certificados"
+              element={<Certificados />}
+            />
+            <Route
+              path="/estudiante/cursos/curso1/unidad1"
+              element={<Unidad1 />}
+            />
+            <Route
+              path="/estudiante/cursos/curso1/unidad2"
+              element={<Unidad2 />}
+            />
+            <Route
+              path="/estudiante/cursos/curso1/unidad3"
+              element={<Unidad3 />}
+            />
+            <Route
+              path="/estudiante/dashboard"
+              element={<DashboardEstudiante />}
+            />
+            <Route
+              path="/estudiante/certificados"
+              element={<CertificadosEstudiante />}
+            />
+            <Route
+              path="/estudiante/cursos/curso1/certificado"
+              element={<Certificado />}
+            />
+            <Route
+              path="/estudiante/cursos/curso1"
+              element={<CursosRedirect />}
+            />
+            <Route
+              path="/profile"
+              element={<Perfil />}
+            />
+            <Route
+              path="/edit-profile"
+              element={<EditarPerfil />}
+            />
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
--- src/views/Perfil.jsx
+++ src/views/Perfil.jsx
@@ -1,138 +1,186 @@
-import {useState} from "react";
-import Header from "../components/HeaderIn";
+import { useState } from "react";
+import ModalUser from "../components/ModalUser";
 import user from "../components/user";
 
 const Perfil = () => {
-	const [showMenu, setShowMenu] = useState(true);
-	const [showModal, setShowModal] = useState(false);
-	return (
-		<div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-900 justify-center">
-			<Header
-				setShowMenu={setShowMenu}
-				setShowModal={setShowModal}
-				name={user.nombre + " " + user.apellido}
-				avatar={user.avatar}
-				colorBg="none"
-				textColor="white"
-			/>
-			<div className="w-100% px-[10rem] py-6">
-				<div className="mb-8">
-					<h1 className="font-bold text-3xl mb-2 text-center dark:text-gray-400">
-						Personal info
-					</h1>
-					<h3 className="text-center text-gray-600 dark:text-gray-400">
-						Basic info, like your name and photo
-					</h3>
-				</div>
+  const [showMenu, setShowMenu] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
-				<div className="border border-gray-200 dark:border-gray-700 rounded-xl p-6 mb-8">
-					<div className="flex justify-between items-center mb-4">
-						<div>
-							<h4 className="font-bold text-xl mb-1 dark:text-gray-400">
-								Profile
-							</h4>
-							<p className="text-sm text-gray-500">
-								Some info may be visible to other people
-							</p>
-						</div>
-						<a
-							href="/edit-profile"
-							className="px-4 py-2 border rounded-md border-gray-300 dark:text-gray-400"
-						>
-							Edit
-						</a>
-					</div>
+  const handleMenuClick = () => {
+    setShowMenu((prevShowMenu) => !prevShowMenu);
+  };
 
-					{/* Photo */}
-					<div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
-						<div className="w-1/5">
-							<h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
-								PHOTO
-							</h6>
-						</div>
-						<div className="w-4/5 flex items-center">
-							<img
-								className="rounded-md w-12 h-12 object-cover"
-								src={user.avatar ? `${user.avatar}` : "avatarDef.png"}
-								alt="profile"
-							/>
-						</div>
-					</div>
+  const handleModalClick = () => {
+    setShowModal((prevShowModal) => !prevShowModal);
+  };
 
-					{/* Name */}
-					<div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
-						<div className="w-1/5">
-							<h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
-								NAME
-							</h6>
-						</div>
-						<div className="w-4/5 dark:text-gray-400">
-							{user.nombre + " " + user.apellido}
-						</div>
-					</div>
+  return (
+    <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-800 justify-center">
+      <header
+        className={`w-[100%] h-[4rem] flex justify-between items-center px-[2rem] shadow-sm`}
+      >
+        <a href="/estudiante/dashboard">
+          <div
+            id="menu-principal"
+            className="flex cursor-pointer items-center"
+            onClick={handleMenuClick}
+          >
+            <span className="material-symbols-outlined text-white">
+              arrow_back_ios
+            </span>
+            <h2 className="ml-[1rem] text-white">Back</h2>
+          </div>
+        </a>
+        <div
+          className="flex cursor-pointer"
+          id="show-modal"
+          onClick={handleModalClick}
+        >
+          <img
+            src={user.avatar}
+            alt="avatar"
+            className="w-[2.5rem] h-[2.5rem] rounded-full object-cover"
+          />
+          <div className="flex items-center">
+            <h2 className="ml-[1rem] text-white">
+              {user.nombre + " " + user.apellido}
+            </h2>
+            <span
+              id="more"
+              className="material-symbols-outlined text-white"
+            >
+              expand_more
+            </span>
+          </div>
+          <div
+            className={`${
+              showModal
+                ? "opacity-100 scale-100 duration-300"
+                : "opacity-0 scale-y-90 duration-200 invisible"
+            } transition-all`}
+          >
+            <ModalUser isOpen={showMenu} />
+          </div>
+        </div>
+      </header>
+      <div className="w-[100%] px-[10rem] py-6">
+        <div className="mb-8">
+          <h1 className="font-bold text-3xl mb-2 text-center dark:text-gray-400">
+            Información Personal
+          </h1>
+          <h3 className="text-center text-gray-600 dark:text-gray-400">
+            Mantén tu información personal actualizada, esto garantizará que los
+            certificados no tengan errores al ser generados o impresos.
+          </h3>
+        </div>
 
-					{/* Bio */}
-					<div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
-						<div className="w-1/5">
-							<h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
-								BIO
-							</h6>
-						</div>
-						<div className="w-4/5 dark:text-gray-400">{user.bio}</div>
-					</div>
+        <div className="border border-gray-200 dark:border-gray-700 rounded-xl p-6 mb-8">
+          <div className="flex justify-between items-center mb-4">
+            <div>
+              <h4 className="font-bold text-xl mb-1 dark:text-gray-400">
+                Perfil
+              </h4>
+              <p className="text-sm text-gray-500">
+                La información personal será visible solamente para ti, no se
+                comparte con ningún tercero.
+              </p>
+            </div>
+            <a
+              href="/edit-profile"
+              className="px-4 py-2 border rounded-md border-gray-300 dark:text-gray-400"
+            >
+              Edit
+            </a>
+          </div>
 
-					{/* Phone */}
-					<div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
-						<div className="w-1/5">
-							<h6 className="text-gray-400 text-xs font-medium dark:text-gray-400">
-								PHONE
-							</h6>
-						</div>
-						<div className="w-4/5 text-gray-400 dark:text-gray-400">
-							{user.phone}
-						</div>
-					</div>
+          {/* Photo */}
+          <div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
+            <div className="w-1/5">
+              <h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
+                FOTO
+              </h6>
+            </div>
+            <div className="w-4/5 flex items-center">
+              <img
+                className="rounded-md w-12 h-12 object-cover"
+                src={user.avatar ? `${user.avatar}` : "avatarDef.png"}
+                alt="profile"
+              />
+            </div>
+          </div>
 
-					{/* Email */}
-					<div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
-						<div className="w-1/5">
-							<h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
-								EMAIL
-							</h6>
-						</div>
-						<div className="w-4/5 dark:text-gray-400">{user.email}</div>
-					</div>
+          {/* Name */}
+          <div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
+            <div className="w-1/5">
+              <h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
+                NOMBRE
+              </h6>
+            </div>
+            <div className="w-4/5 dark:text-gray-400">
+              {user.nombre + " " + user.apellido}
+            </div>
+          </div>
 
-					{/* Password */}
-					<div className="flex items-center">
-						<div className="w-1/5">
-							<h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
-								PASSWORD
-							</h6>
-						</div>
-						<div className="w-4/5 dark:text-gray-400">
-							{Array(user.password.length).fill("•").join("")}
-						</div>
-					</div>
-				</div>
+          {/* Bio */}
+          <div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
+            <div className="w-1/5">
+              <h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
+                BIO
+              </h6>
+            </div>
+            <div className="w-4/5 dark:text-gray-400">{user.bio}</div>
+          </div>
 
-				<div className="flex justify-between items-center mt-4">
-					<div>
-						<p className="text-gray-600 text-xs">created by</p>
-						<a
-							href="https://github.com/kevinvillajim"
-							className="text-blue-500 text-xs font-medium"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							kevinvillajim
-						</a>
-					</div>
-					<div className="text-xs dark:text-gray-600">Sinergy Hard ©</div>
-				</div>
-			</div>
-		</div>
-	);
+          {/* Phone */}
+          <div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
+            <div className="w-1/5">
+              <h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
+                TELÉFONO
+              </h6>
+            </div>
+            <div className="w-4/5 dark:text-gray-400">{user.phone}</div>
+          </div>
+
+          {/* Email */}
+          <div className="flex items-center border-b border-gray-200 dark:border-gray-700 pb-3 mb-3">
+            <div className="w-1/5">
+              <h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
+                EMAIL
+              </h6>
+            </div>
+            <div className="w-4/5 dark:text-gray-400">{user.email}</div>
+          </div>
+
+          {/* Password */}
+          <div className="flex items-center">
+            <div className="w-1/5">
+              <h6 className="text-gray-500 text-xs font-medium dark:text-gray-400">
+                PASSWORD
+              </h6>
+            </div>
+            <div className="w-4/5 dark:text-gray-400">
+              {Array(user.password.length).fill("•").join("")}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-between items-center mt-4">
+          <div>
+            <p className="text-gray-600 text-xs">created by</p>
+            <a
+              href="https://github.com/kevinvillajim"
+              className="text-blue-500 text-xs font-medium"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              kevinvillajim
+            </a>
+          </div>
+          <div className="text-xs dark:text-gray-600">Sinergy Hard ©</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Perfil;
